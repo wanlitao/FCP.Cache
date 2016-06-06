0968a86b7ce8d938ad2e6c9ac4d905c8847d77f1
万里涛 <wanlitao_3055601@outlook.com>
2016-06-06 18:10:03 +0800

    add concurrent task manager

---
+++ FCP.Cache.Service/ConcurrentTaskManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace FCP.Cache.Service
+{
+    public class ConcurrentTaskManager
+    {
+        private readonly ConcurrentDictionary<string, Func<Task>> _taskDict = new ConcurrentDictionary<string, Func<Task>>();
+
+        public Task GetOrAdd(string key, Func<Task> taskFunc)
+        {
+            Func<Task> newTaskFunc = () =>
+            {
+                var task = taskFunc();
+                task.ContinueWith((t) => { TryRemove(key); });
+                return task;
+            };
+
+            var dictTaskFunc = _taskDict.GetOrAdd(key, newTaskFunc);
+            return dictTaskFunc();
+        }
+
+        public bool TryRemove(string key)
+        {
+            Func<Task> taskFunc;
+            return _taskDict.TryRemove(key, out taskFunc);
+        }
+    }
+}
